92bad3cfc101012986010fd8153331ab150293ab
pritamsenweb <pritam_senpapai@rediffmail.com>
2023-06-25 16:29:08 +0530

    Text Updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from './component/Hero'
 export default function Home() {
   return (
     <>
-    <Hero title={"LET'S WATCH MOVIE TOGETHER"} shortdes={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."} imageUrl ={"./about1.svg"} />
+    <Hero title={"LETs WATCH MOVIE TOGETHER"} shortdes={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."} imageUrl ={"./about1.svg"} />
     </>
   )
 }
