bd403f8ba15dad004b09ed3755a4a6997f64c216
pritamsenweb <pritam_senpapai@rediffmail.com>
2023-06-25 16:32:49 +0530

    textUpdate

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -9,7 +9,7 @@ const Contact = () => {
         <h1>Contact Us</h1>
         <ContactCard />
         <section className={styles.contact_section}>
-          <h2>We'd love to head <span>from you</span></h2>
+          <h2>We would love to head <span>from you</span></h2>
           <ContactForm />
         </section>
         
